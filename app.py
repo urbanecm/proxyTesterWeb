a9495e3a9d8f72d1c16807115c6701018252437e
Martin Urbanec <martin@urbanec.cz>
2017-04-28 15:37:49 +0200

    Remove untrue info

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of the Tool Labs flask WSGI tutorial
-#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
