c30314154ba7ba03213a8c9209fcf021fa43cd29
Martin Urbanec <martin@urbanec.cz>
2017-04-28 15:37:22 +0200

    Remove untrue copyright info

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 #
 # This file is part of the Tool Labs flask WSGI tutorial
 #
-# Copyright (C) 2017 Bryan Davis and contributors
-#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
