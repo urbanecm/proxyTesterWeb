3e67aee3d2c1269e5b154b69efda369ab18344eb
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-28 17:17:44 +0200

    quick fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,8 @@
-var ip = $('#ip').val();
-var port = $('#port').val();
-
 testProxy = function {
+
+	var ip = $('#ip').val();
+	var port = $('#port').val();
+
 	if (ip != '')
 	{
 		$.get("https://tools.wmflabs.org/proxies/checkProxy?ip=" + ip + "?port=" + port function ( respound ) {
@@ -14,6 +15,7 @@ $( document ).ready(function () {
 
 	$('#testProxy').click(function () {
 		testProxy();
+		console.log('testing proxy');
 	});
 
 });
