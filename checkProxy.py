469fdf4a31b6c6f52436da55132bd6ff703137af
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-30 16:45:58 +0200

    1.0V

---
+++ checkProxy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import urllib
+import re
+import sys
+
+def check_proxy(ip, port):
+	try:
+		proxy = {'http' : 'http//' + ip + ':' + str(port)}
+		proxy = urllib.urlopen("http://httpbin.org/ip", proxies=proxy).read()
+		proxy = re.findall(r'"([^"]*)"', proxy)
+		if ip in proxy:
+			return True
+		else:
+			return False
+		except KeyboardInterrupt:
+			print "Ctrl+c was pressed. Exiting!!!"
+			sys.exit(0)
+		except:
+			return "connection lost"
